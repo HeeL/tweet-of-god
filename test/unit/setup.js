27258ea7f3ecac67b027324aaa1ae6f1a5a80257
HeeL <parizhskiy@gmail.com>
2018-02-20 06:38:25 +0100

    Add enzyme dependencies and configuration

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,3 +1,8 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 process.once('unhandledRejection', (error) => {
     throw new Error(`Unhandled rejection: ${error.stack}`);
 });
