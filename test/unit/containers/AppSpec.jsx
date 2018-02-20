a6cede768c7524e44460719b95261117a1a7063a
HeeL <parizhskiy@gmail.com>
2018-02-20 06:38:49 +0100

    Add simple test for App.jsx with enzyme

--- test/unit/containers/AppSpec.jsx
+++ test/unit/containers/AppSpec.jsx
@@ -1,7 +1,11 @@
+import React from 'react';
+import {shallow} from 'enzyme';
 import App from '../../../containers/App';
 
 describe('App', () => {
     it('should render', () => {
-        
+        const app = shallow(<App />);
+
+        expect(app).toHaveLength(1);
     });
-})
+});
