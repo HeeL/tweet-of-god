10f7b249ff16cd80182585904a16adec688df23e
HeeL <parizhskiy@gmail.com>
2018-03-02 10:30:04 +0100

    Add input field and submit button
    - persist input in state
    - console.log text on submit for now

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => (
-    <div>
-        Hello
-    </div>
-);
+export default class App extends Component {
+    constructor(args) {
+        super(...args);
+        this.state = { tweetText: '' };
+        this.updateTweetText = this.updateTweetText.bind(this);
+        this.sendTweet = this.sendTweet.bind(this);
+    }
+
+    sendTweet(event) {
+        event.preventDefault();
+        console.log(this.state.tweetText);
+    }
+
+    updateTweetText(event) {
+        this.setState({ tweetText: event.target.value });
+    }
+
+    render() {
+        return (
+            <form>
+                <input
+                    type="text"
+                    name="tweet"
+                    onChange={this.updateTweetText}
+                    value={this.state.tweetText}
+                />
+
+                <input
+                    type="submit"
+                    onClick={this.sendTweet}
+                    value="Tweet It!"
+                />
+            </form>
+        );
+    }
+}
--- test/unit/containers/AppSpec.jsx
+++ test/unit/containers/AppSpec.jsx
@@ -3,9 +3,23 @@ import { shallow } from 'enzyme';
 import App from '../../../app/containers/App';
 
 describe('App', () => {
-    it('should render', () => {
+    it('renders', () => {
         const app = shallow(<App />);
 
         expect(app).toHaveLength(1);
+    });
+
+    it('contains input field', () => {
+        const app = shallow(<App />);
+        const input = app.find('input[type="text"]');
+
+        expect(input).toHaveLength(1);
+    });
+
+    it('contains submit button', () => {
+        const app = shallow(<App />);
+        const input = app.find('input[type="submit"]');
+
+        expect(input).toHaveLength(1);
     });
 });
