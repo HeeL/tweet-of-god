bbe5274258273c29cd072dfc04bc5328dfa715d0
HeeL <parizhskiy@gmail.com>
2018-03-03 04:58:44 +0100

    Use camel case keys for style properties

--- app/containers/styles.jsx
+++ app/containers/styles.jsx
@@ -2,27 +2,27 @@ import glamorous from 'glamorous';
 
 export const Form = glamorous.form({
     display: 'flex',
-    'flex-wrap': 'wrap',
-    'justify-content': 'center'
+    flexWrap: 'wrap',
+    justifyContent: 'center'
 });
 
 export const Input = glamorous.input({
     width: '100%',
     margin: '20px',
     padding: '5px',
-    'font-size': '18px'
+    fontSize: '18px'
 });
 
 export const Submit = glamorous.input({
-    'background-image': 'linear-gradient(to bottom, #3498db, #2980b9)',
-    'font-size': '20px',
-    'text-decoration': 'none',
+    backgroundImage: 'linear-gradient(to bottom, #3498db, #2980b9)',
+    fontSize: '20px',
+    textDecoration: 'none',
     color: '#fff',
     padding: '10px 20px 10px 20px',
     background: '#3498db',
     ':hover': {
         background: '#3cb0fd',
-        'background-image': 'linear-gradient(to bottom, #3cb0fd, #3498db)',
-        'text-decoration': 'none'
+        backgroundImage: 'linear-gradient(to bottom, #3cb0fd, #3498db)',
+        textDecoration: 'none'
     }
 });
