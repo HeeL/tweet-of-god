78ebf4c69902c9196f50e08a88c806b0a4090d09
HeeL <parizhskiy@gmail.com>
2018-03-06 05:30:25 +0100

    Use document from window and remove eslint-disable comment

--- index.jsx
+++ index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/components/App';
 
-// eslint-disable-next-line no-undef
 const props = { window, fetch };
 
-// eslint-disable-next-line no-undef
-ReactDOM.render(<App {...props} />, document.getElementById('app'));
+ReactDOM.render(<App {...props} />, window.document.getElementById('app'));
