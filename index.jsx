c70d5978e8cd738cbec6dcfe5ec65247b64bafdd
HeeL <parizhskiy@gmail.com>
2018-03-06 05:11:45 +0100

    Disable eslint no-undef instead of disabling completely

--- index.jsx
+++ index.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/components/App';
 
-// eslint-disable-next-line
+// eslint-disable-next-line no-undef
 ReactDOM.render(<App />, document.getElementById('app'));
